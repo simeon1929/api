0564e9a68b3dad28cd98cede78348afd721d191d
Simeon Haries <simeon@voxforem.com>
2021-12-17 00:11:51 +0530

    api get

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 
 
 async function apIget (){
-    var getApi = await fetch('http://dummy.restapiexample.com/api/v1/employees')
+    try {
+    var getApi = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if(getApi.ok===false){
+        throw 'page not found'
+    }
     var conJason = await getApi.json()
+    //return conJason;
     console.log(conJason);
-}
+    }
+    catch(err){
+        console.log(err);
+        console.log('through Erro');
+    }
 
-apIget()
+    
+}
+apIget ()
